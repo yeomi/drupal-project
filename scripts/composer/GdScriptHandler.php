66c0e93be7c7cc72afdb8b9130821f26c32bacc9
Gabriel Henao <gabriel@henao.fr>
2019-11-19 17:32:26 +0100

    first commit

---
+++ scripts/composer/GdScriptHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalProject\composer\ScriptHandler.
+ */
+
+namespace DrupalProject\composer;
+
+use Composer\Script\Event;
+use Composer\Semver\Comparator;
+use DrupalFinder\DrupalFinder;
+use Symfony\Component\Filesystem\Filesystem;
+use Webmozart\PathUtil\Path;
+
+class GdScriptHandler extends ScriptHandler {
+
+
+  private static function createDirectories($dirs) {
+
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+
+    foreach ($dirs as $dir => $subDirs) {
+      if (!$fs->exists($drupalRoot . '/'. $dir)) {
+        $fs->mkdir($drupalRoot . '/'. $dir);
+        $fs->touch($drupalRoot . '/'. $dir . '/.gitkeep');
+      }
+      self::createDirectories($subDirs);
+    }
+  }
+
+  public static function createRequiredFiles(Event $event) {
+
+    parent::createRequiredFiles($event);
+
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+
+    $dirs = [
+      'private' => [],
+      'temp' => [],
+      'config' => [
+        'dev' => [],
+        'prod' => [],
+        'preprod' => [],
+        'ignore' => [],
+        'sync' => [],
+      ],
+    ];
+
+    self::createDirectories($dirs);
+
+    if (!$fs->exists($drupalRoot . '/themes/custom/app_theme') && $fs->exists($drupalFinder->getComposerRoot() . '/vendor/php-packages/drupal8-theme')) {
+      $fs->copy($drupalFinder->getComposerRoot() . '/vendor/php-packages/drupal8-theme', $drupalRoot . '/themes/custom/app_theme');
+    }
+  }
+
+
+
+}
